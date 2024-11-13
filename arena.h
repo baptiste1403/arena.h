a47018961bc33b783160528c38dbbb49b5a820d1
Baptiste Asselin <baptiste.asselin@soprasteria.com>
2024-11-13 09:05:12 +0100

    update license

--- arena.h
+++ arena.h
@@ -1,6 +1,6 @@
 /**
  * MIT License
- * Copyright (c) 2024 Blackcat
+ * Copyright (c) 2024 Baptiste ASSELIN
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
